fd5290b8e1526cc0c220d7d14df1a2db96a5375d
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-03 10:17:07 +0200

    describe the writeContacts utility function

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const JSON_PARSE_CONFIG = {
+    encoding: 'utf8', // for adding other configurations
+  };
+
+  const data = JSON.stringify(updatedContacts, null, 2);
+  try {
+    await writeFile(PATH_DB, data, JSON_PARSE_CONFIG);
+  } catch (error) {
+    console.error('Помилка читання файлу: ', error);
+  }
+};
