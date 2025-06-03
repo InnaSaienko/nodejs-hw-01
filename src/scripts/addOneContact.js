5453cf8a4415251e9d8781183a9cc08f25e39cb2
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-03 10:49:14 +0200

    describe the addOneContact function

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,16 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const addOneContact = async () => {
+  let contacts = await readContacts();
+  contacts.push(createFakeContact());
+
+  try {
+    await writeContacts(contacts);
+  } catch (e) {
+    console.log('add one Contact error: ', e);
+  }
+};
 
 addOneContact();
