904f6f826cbf0c439b438a60bf3cb7703930b4c3
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-03 10:35:41 +0200

    describe the generateContacts function

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,20 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  let contacts = await readContacts();
+  let i = 1;
+  while (i <= number) {
+    contacts.push(createFakeContact());
+    i++;
+  }
+
+  try {
+    await writeContacts(contacts);
+  } catch (e) {
+    console.log('generate Contacts error: ', e);
+  }
+};
 
 generateContacts(5);
