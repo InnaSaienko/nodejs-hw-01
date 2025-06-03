0d86536401be7140eeea27a6ebfdb8c09728d766
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-03 10:16:44 +0200

    describe the readContacts utility function

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const JSON_PARSE_CONFIG = {
+    encoding: 'utf8', // for adding other configurations
+  };
+
+  try {
+    const data = await readFile(PATH_DB, JSON_PARSE_CONFIG);
+    if (!data) return [];
+    const parsedData = JSON.parse(data);
+    if (!Array.isArray(parsedData)) return [];
+    return parsedData;
+
+  } catch (e) {
+    console.error('File reading error: ', e);
+    return [];
+  }
+};
