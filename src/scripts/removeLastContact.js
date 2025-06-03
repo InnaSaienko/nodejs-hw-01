2dde33c7906cbb03e21acb8e440982d52d12b18e
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-03 11:00:09 +0200

    describe the removeLastContact function

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,11 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  let contacts = await readContacts();
+  contacts.pop();
+  return await writeContacts(contacts);
+};
+
 
 removeLastContact();
